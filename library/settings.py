fb8598ae0afec3ff99b1e96e968f82555a867ba9
Md Masipul Islam Siam <masipulislam@gmail.com>
2024-01-07 00:31:07 +0600

    library project v-01

--- library/settings.py
+++ library/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f3sw0e77(7u*+(e0(6fg)i6pt7@h+vg_q@ocq0=kju(y7h#@qz'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
